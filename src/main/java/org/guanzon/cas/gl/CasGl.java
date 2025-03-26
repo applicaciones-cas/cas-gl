f44313ce5f95700df00edb83735aa868d78267e8
xurpas7 <michael_cuison07@yahoo.com>
2025-03-26 16:09:10 +0800

    Initial commit

---
+++ src/main/java/org/guanzon/cas/gl/CasGl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.guanzon.cas.gl;
+
+/**
+ *
+ * @author micha
+ */
+public class CasGl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
