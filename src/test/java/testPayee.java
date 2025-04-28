4c002ec94d922d152ac8bdbeb09113c5dafe517d
itsjohndavea <johndaveaquino35@gmail.com>
2025-04-28 17:18:08 +0800

    Create testPayee.java

---
+++ src/test/java/testPayee.java
@@ -0,0 +1,109 @@
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.guanzon.appdriver.base.GRiderCAS;
+import org.guanzon.appdriver.base.GuanzonException;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.cas.gl.services.GLControllers;
+import org.json.simple.JSONObject;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author User
+ */
+public class testPayee {
+
+    static GRiderCAS oApp;
+    static GLControllers poGLControllers;
+    static JSONObject poJSON;
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.setProperty("sys.default.path.metadata", "D:/GGC_Maven_Systems/config/metadata/new/");
+
+        oApp = MiscUtil.Connect();
+        poGLControllers = new GLControllers(oApp, null);
+    }
+
+    @Test
+    public void testNewRecord() {
+
+        try {
+            poJSON = poGLControllers.Payee().newRecord();
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+            poJSON = poGLControllers.Payee().getModel().setPayeeName("CENPELCO");
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+
+            poJSON = poGLControllers.Payee().getModel().setParticularID("0001");
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+            poJSON = poGLControllers.Payee().saveRecord();
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+        } catch (SQLException | GuanzonException | CloneNotSupportedException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testUpdateRecord() {
+        try {
+            JSONObject poJSON;
+            poJSON = poGLControllers.Payee().openRecord("24009");
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+
+            poJSON = poGLControllers.Payee().updateRecord();
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+            poJSON = poGLControllers.Payee().getModel().setPayeeName("CENPELCO");
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+
+            poJSON = poGLControllers.Payee().getModel().setParticularID("0001");
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+            poJSON = poGLControllers.Payee().getModel().setModifyingId(oApp.getUserID());
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+
+            poJSON = poGLControllers.Payee().getModel().setModifiedDate(oApp.getServerDate());
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+
+            poJSON = poGLControllers.Payee().saveRecord();
+            if ("error".equals((String) poJSON.get("result"))) {
+                Assert.fail((String) poJSON.get("message"));
+            }
+        } catch (SQLException | GuanzonException | CloneNotSupportedException ex) {
+            Logger.getLogger(testPayee.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        poGLControllers = null;
+        oApp = null;
+    }
+}
