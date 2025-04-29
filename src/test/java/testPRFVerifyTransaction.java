237a96d301389bfbd2dc555c4febfd25ac85c9f6
xurpas7 <michael_cuison07@yahoo.com>
2025-04-29 14:52:36 +0800

    20250429

--- src/test/java/testPRFVerifyTransaction.java
+++ src/test/java/testPRFVerifyTransaction.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.guanzon.appdriver.base.GRiderCAS;
 import org.guanzon.appdriver.base.GuanzonException;
 import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.cas.gl.PaymentRequest;
 import org.guanzon.cas.gl.services.GLControllers;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -20,7 +21,7 @@ public class testPRFVerifyTransaction {
 
     
     static GRiderCAS poApp;
-    static GLControllers poPaymentRequest;
+    static PaymentRequest poPaymentRequest;
 
     @BeforeClass
     public static void setUpClass() {
@@ -28,7 +29,14 @@ public class testPRFVerifyTransaction {
 
         poApp = MiscUtil.Connect();
 
-        poPaymentRequest = new GLControllers(poApp, null);
+        try {
+            poPaymentRequest = new GLControllers(poApp, null).PaymentRequest();
+            poPaymentRequest.setBranchCode(poApp.getBranchCode());
+        } catch (SQLException | GuanzonException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+        
     }
     
 
@@ -37,13 +45,13 @@ public class testPRFVerifyTransaction {
         JSONObject loJSON;
 
         try {
-            loJSON = poPaymentRequest.PaymentRequest().InitTransaction();
+            loJSON = poPaymentRequest.InitTransaction();
             if (!"success".equals((String) loJSON.get("result"))) {
                 System.err.println((String) loJSON.get("message"));
                 Assert.fail();
             }
 
-            loJSON = poPaymentRequest.PaymentRequest().OpenTransaction("GCC225000001");
+            loJSON = poPaymentRequest.OpenTransaction("GCC225000001");
             if (!"success".equals((String) loJSON.get("result"))) {
                 System.err.println((String) loJSON.get("message"));
                 Assert.fail();
@@ -51,26 +59,26 @@ public class testPRFVerifyTransaction {
 
 
 
-            System.out.println("Transaction No: " + poPaymentRequest.PaymentRequest().Master().getTransactionNo());
-            System.out.println("Transaction Date : " + poPaymentRequest.PaymentRequest().Master().getTransactionDate().toString());
-            System.out.println("Branch: " + poPaymentRequest.PaymentRequest().Master().Branch().getBranchName());
-            System.out.println("Department: " + poPaymentRequest.PaymentRequest().Master().Department().getDescription());
+            System.out.println("Transaction No: " + poPaymentRequest.Master().getTransactionNo());
+            System.out.println("Transaction Date : " + poPaymentRequest.Master().getTransactionDate().toString());
+            System.out.println("Branch: " + poPaymentRequest.Master().Branch().getBranchName());
+            System.out.println("Department: " + poPaymentRequest.Master().Department().getDescription());
 //            System.out.println("Payee: " + poPaymentRequest.PaymentRequest().Master().getPayeeID());
-            System.out.println("Series No: " + poPaymentRequest.PaymentRequest().Master().getSeriesNo());
-            System.out.println("Entry No: " + poPaymentRequest.PaymentRequest().Master().getEntryNo());
+            System.out.println("Series No: " + poPaymentRequest.Master().getSeriesNo());
+            System.out.println("Entry No: " + poPaymentRequest.Master().getEntryNo());
             System.out.println("");
-            int detailSize = poPaymentRequest.PaymentRequest().Detail().size();
+            int detailSize = poPaymentRequest.Detail().size();
             if (detailSize > 0) {
-                 for (int lnCtr = 0; lnCtr < poPaymentRequest.PaymentRequest().Detail().size(); lnCtr++) {
+                 for (int lnCtr = 0; lnCtr < poPaymentRequest.Detail().size(); lnCtr++) {
                     System.out.println("DETAIL------------------- " + (lnCtr + 1));
-                    System.out.println("TRANSACTION NO : " + poPaymentRequest.PaymentRequest().Master().getTransactionNo());
-                    System.out.println("ENTRY No: " + poPaymentRequest.PaymentRequest().Detail(lnCtr).getEntryNo());
-                    System.out.println("PARTICULAR ID : " + poPaymentRequest.PaymentRequest().Detail(lnCtr).getParticularID());
+                    System.out.println("TRANSACTION NO : " + poPaymentRequest.Master().getTransactionNo());
+                    System.out.println("ENTRY No: " + poPaymentRequest.Detail(lnCtr).getEntryNo());
+                    System.out.println("PARTICULAR ID : " + poPaymentRequest.Detail(lnCtr).getParticularID());
                     System.out.println("");
                  }
             }
 
-            loJSON = poPaymentRequest.PaymentRequest().ConfirmTransaction("verify");
+            loJSON = poPaymentRequest.ConfirmTransaction("verify");
             if (!"success".equals((String) loJSON.get("result"))) {
                 System.err.println((String) loJSON.get("message"));
                 Assert.fail();
@@ -78,8 +86,7 @@ public class testPRFVerifyTransaction {
 
 //            System.out.println((String) loJSON.get("message"));
         } catch (CloneNotSupportedException | SQLException | GuanzonException | ParseException e) {
-            System.err.println(MiscUtil.getException(e));
-            Logger.getLogger(MiscUtil.getException(e));
+            MiscUtil.getException(e);
             Assert.fail();
         
         } 
