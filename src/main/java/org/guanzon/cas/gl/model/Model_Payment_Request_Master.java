11cd4daa4748b442a0810ba7384d2b03ffd923c6
itsjohndavea <johndaveaquino35@gmail.com>
2025-04-30 16:23:38 +0800

    update departmentid

--- src/main/java/org/guanzon/cas/gl/model/Model_Payment_Request_Master.java
+++ src/main/java/org/guanzon/cas/gl/model/Model_Payment_Request_Master.java
@@ -23,24 +23,23 @@ import org.json.simple.JSONObject;
  *
  * @author User
  */
-public class Model_Payment_Request_Master extends Model{
-    Model_Department poDepartment;    
-    Model_Payee poPayee;    
+public class Model_Payment_Request_Master extends Model {
+
+    Model_Department poDepartment;
+    Model_Payee poPayee;
     Model_Branch poBranch;
-    
-    
+
     @Override
     public void initialize() {
         try {
             poEntity = MiscUtil.xml2ResultSet(System.getProperty("sys.default.path.metadata") + XML, getTable());
-            
+
             poEntity.last();
             poEntity.moveToInsertRow();
 
             MiscUtil.initRowSet(poEntity);
-            
+
             //assign default values
- 
             poEntity.updateString("cTranStat", PaymentRequestStatus.OPEN);
             poEntity.updateObject("nTranTotl", 0.0);
             poEntity.updateObject("nDiscAmtx", 0.0);
@@ -57,7 +56,7 @@ public class Model_Payment_Request_Master extends Model{
             poEntity.absolute(1);
 
             ID = "sTransNox";
-            
+
             //initialize reference objects
             ParamModels model = new ParamModels(poGRider);
             poDepartment = model.Department();
@@ -67,181 +66,179 @@ public class Model_Payment_Request_Master extends Model{
 //            poCategory = model.Category();
 //            poCompany = model.Company();
 //            poTerm = model.Term();
-//            
-//            ClientModel clientModel = new ClientModel(poGRider); 
+//
+//            ClientModel clientModel = new ClientModel(poGRider);
 //            poSupplier = clientModel.ClientMaster();
 //            poSupplierAdress = clientModel.ClientAddress();
 //            poSupplierContactPerson = clientModel.ClientInstitutionContact();
-            
-            
-           
+
             //end - initialize reference objects
-            
             pnEditMode = EditMode.UNKNOWN;
         } catch (SQLException e) {
             logwrapr.severe(e.getMessage());
             System.exit(1);
         }
     }
-    
+
     private static String xsDateShort(Date fdValue) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String date = sdf.format(fdValue);
         return date;
     }
-    
-    public JSONObject setTransactionNo(String transactionNo){
+
+    public JSONObject setTransactionNo(String transactionNo) {
         return setValue("sTransNox", transactionNo);
     }
-    
-    public String getTransactionNo  (){
+
+    public String getTransactionNo() {
         return (String) getValue("sTransNox");
     }
-    
-    public JSONObject setTransactionDate(Date transactionDate){
+
+    public JSONObject setTransactionDate(Date transactionDate) {
         return setValue("dTransact", transactionDate);
     }
-    
-    public Date getTransactionDate(){
+
+    public Date getTransactionDate() {
         return (Date) getValue("dTransact");
     }
-    
-    public JSONObject setBranchCode(String branchCode){
+
+    public JSONObject setBranchCode(String branchCode) {
         return setValue("sBranchCd", branchCode);
     }
-    
-    public String getBranchCode(){
+
+    public String getBranchCode() {
         return (String) getValue("sBranchCd");
     }
-    
-    public JSONObject setDepartmentID(String deptartmentID){
+
+    public JSONObject setDepartmentID(String deptartmentID) {
         return setValue("sDeptIDxx", deptartmentID);
     }
-    
-    public String getDepartmentID(){
+
+    public String getDepartmentID() {
         return (String) getValue("sDeptIDxx");
     }
-    
-    public JSONObject setPayeeID(String payeeID){
+
+    public JSONObject setPayeeID(String payeeID) {
         return setValue("sPayeeIDx", payeeID);
     }
-    
-    public String getPayeeID(){
+
+    public String getPayeeID() {
         return (String) getValue("sPayeeIDx");
     }
-    
-    public JSONObject setSource(String source){
+
+    public JSONObject setSource(String source) {
         return setValue("cSourcexx", source);
     }
-    
-    public String getSource(){
+
+    public String getSource() {
         return (String) getValue("cSourcexx");
     }
-    
-    public JSONObject setSeriesNo(String seriesNo){
+
+    public JSONObject setSeriesNo(String seriesNo) {
         return setValue("sSeriesNo", seriesNo);
     }
-    
-    public String getSeriesNo(){
+
+    public String getSeriesNo() {
         return (String) getValue("sSeriesNo");
     }
-    
-    public JSONObject setTranTotal(Number tranTotal){
+
+    public JSONObject setTranTotal(Number tranTotal) {
         return setValue("nTranTotl", tranTotal);
     }
-    
-    public Number getTranTotal(){
+
+    public Number getTranTotal() {
         return (Number) getValue("nTranTotl");
     }
-    
-    public JSONObject setDiscountAmount(Number discountAmount){
+
+    public JSONObject setDiscountAmount(Number discountAmount) {
         return setValue("nDiscAmtx", discountAmount);
     }
-    
-    public Number getDiscountAmount(){
+
+    public Number getDiscountAmount() {
         return (Number) getValue("nDiscAmtx");
     }
-    
-    public JSONObject setTaxAmount(Number taxAmount){
+
+    public JSONObject setTaxAmount(Number taxAmount) {
         return setValue("nTaxAmntx", taxAmount);
     }
-    
-    public Number getTaxAmount(){
+
+    public Number getTaxAmount() {
         return (Number) getValue("nTaxAmntx");
     }
-    
-    public JSONObject setNetTotal(Number netTotal){
+
+    public JSONObject setNetTotal(Number netTotal) {
         return setValue("nNetTotal", netTotal);
     }
-    
-    public Number getNetTotal(){
+
+    public Number getNetTotal() {
         return (Number) getValue("nNetTotal");
     }
-    
-    public JSONObject setAmountPaid(Number amountPaid){
+
+    public JSONObject setAmountPaid(Number amountPaid) {
         return setValue("nAmtPaidx", amountPaid);
     }
-    
-    public Number getAmountPaid(){
+
+    public Number getAmountPaid() {
         return (Number) getValue("nAmtPaidx");
     }
-    
-    public JSONObject setEntryNo(int entryNo){
+
+    public JSONObject setEntryNo(int entryNo) {
         return setValue("nEntryNox", entryNo);
     }
-    
-    public int getEntryNo(){
+
+    public int getEntryNo() {
         return (int) getValue("nEntryNox");
     }
-    
-    public JSONObject setSourceCode(String sourceCode){
+
+    public JSONObject setSourceCode(String sourceCode) {
         return setValue("sSourceCd", sourceCode);
     }
-    
-    public String getSourceCode(){
+
+    public String getSourceCode() {
         return (String) getValue("sSourceCd");
     }
-    
-    public JSONObject setSourceNo(String sourceNo){
+
+    public JSONObject setSourceNo(String sourceNo) {
         return setValue("sSourceNo", sourceNo);
     }
-    
-    public String getSourceNo(){
+
+    public String getSourceNo() {
         return (String) getValue("sSourceNo");
     }
-    
-    public JSONObject setTransactionStatus(String transactionStatus){
+
+    public JSONObject setTransactionStatus(String transactionStatus) {
         return setValue("cTranStat", transactionStatus);
     }
-    
-    public String getTransactionStatus(){
+
+    public String getTransactionStatus() {
         return (String) getValue("cTranStat");
     }
-    
-    public JSONObject setRemarks(String remarks){
+
+    public JSONObject setRemarks(String remarks) {
         return setValue("sRemarksx", remarks);
     }
-    
-    public String getRemarks(){
+
+    public String getRemarks() {
         return (String) getValue("sRemarksx");
     }
-    
-    public JSONObject setModifyingId(String modifyingId){
+
+    public JSONObject setModifyingId(String modifyingId) {
         return setValue("sModified", modifyingId);
     }
-    
-    public String getModifyingId(){
+
+    public String getModifyingId() {
         return (String) getValue("sModified");
     }
-    
-    public JSONObject setModifiedDate(Date modifiedDate){
+
+    public JSONObject setModifiedDate(Date modifiedDate) {
         return setValue("dModified", modifiedDate);
     }
-    
-    public Date getModifiedDate(){
+
+    public Date getModifiedDate() {
         return (Date) getValue("dModified");
     }
-        @Override
+
+    @Override
     public String getNextCode() {
         return MiscUtil.getNextCode(this.getTable(), ID, true, poGRider.getGConnection().getConnection(), poGRider.getBranchCode());
     }
@@ -249,10 +246,10 @@ public class Model_Payment_Request_Master extends Model{
     public Model_Department Department() throws GuanzonException, SQLException {
         if (!"".equals((String) getValue("sDeptIDxx"))) {
             if (poDepartment.getEditMode() == EditMode.READY
-                    && poDepartment.getDepartmentId().equals((String) getValue("sDeptIdxx"))) {
+                    && poDepartment.getDepartmentId().equals((String) getValue("sDeptIDxx"))) {
                 return poDepartment;
             } else {
-                poJSON = poDepartment.openRecord((String) getValue("sDeptIdxx"));
+                poJSON = poDepartment.openRecord((String) getValue("sDeptIDxx"));
                 if ("success".equals((String) poJSON.get("result"))) {
                     return poDepartment;
                 } else {
@@ -285,7 +282,7 @@ public class Model_Payment_Request_Master extends Model{
             return poPayee;
         }
     }
-    
+
     public Model_Branch Branch() throws GuanzonException, SQLException {
         if (!"".equals((String) getValue("sBranchCd"))) {
             if (poBranch.getEditMode() == EditMode.READY
